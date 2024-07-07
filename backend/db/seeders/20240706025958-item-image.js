d612b724ae5e528c0e2f6aa336cc782de816ccbf
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-07-07 14:16:43 -0700

    Seed corresponding images for Items

--- backend/db/seeders/20240706025958-item-image.js
+++ backend/db/seeders/20240706025958-item-image.js
@@ -14,43 +14,43 @@ const demoItemImages = [
   },
   {
     itemId: 2,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/1tLQ11720385984.jpg'
   },
   {
     itemId: 3,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/bo9691720385994.jpg'
   },
   {
     itemId: 4,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/0MM8v1720386051.jpg'
   },
   {
     itemId: 5,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/WmJEb1720386653.jpg'
   },
   {
     itemId: 6,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/GuZko1720386691.jpg'
   },
   {
     itemId: 7,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/e1jZP1720386729.jpg'
   },
   {
     itemId: 8,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/QUDdO1720386741.jpg'
   },
   {
     itemId: 9,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/SBZhO1720386784.jpg'
   },
   {
     itemId: 10,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/HvdZ21720386793.jpg'
   },
   {
     itemId: 11,
-    url: 'https://i.imghippo.com/files/MmbZZ1720152675.jpg'
+    url: 'https://i.imghippo.com/files/NuZnD1720386802.jpg'
   },
 ];
 
