7cf97340a6e4bdbc54f4535bf495584735caf4fd
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-07-07 22:52:34 -0700

    Add a SearchBar, and style it

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,8 +1,8 @@
+import './LoginFormModal.css';
 import { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
-import './LoginFormModal.css';
 
 function LoginFormModal() {
     const dispatch = useDispatch();
---
+++ frontend/src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,5 @@
+import './SearchResults.css';
+
+const SearchResults = () => {};
+
+export default SearchResults;
