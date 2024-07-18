03a9dbeda5e6ab3c97316cc41e90411201f7a1e7
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-07-17 17:33:16 -0700

    Add Review backend routes

---
+++ backend/routes/api/review.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const { requireAuth } = require('../../utils/auth.js');
+const { Review, Store } = require('../../db/models');
+
+// --------------------------------------------------------------------------------------//
+//                                   Update a Review                                    //
+// ------------------------------------------------------------------------------------//
+router.put('/:reviewId', requireAuth, async (req, res, next) => {
+    const { reviewId } = req.params;
+    const { body, stars } = req.body;
+
+    try {
+        let review = await Review.findByPk(reviewId);
+        if (!review) {
+            return res.status(404).json({ message: "Review not found" });
+        }
+
+        // Check if the Review belongs to the Current User
+        if (review.userId !== req.user.id) {
+            return res.status(403).json({ message: "Unauthorized. You can only modify your own reviews." });
+        }
+
+        await review.update({ body, stars });
+        return res.status(200).json(review);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// --------------------------------------------------------------------------------------//
+//                                   Delete a Review                                    //
+// ------------------------------------------------------------------------------------//
+router.delete('/:reviewId', requireAuth, async (req, res, next) => {
+    const { reviewId } = req.params;
+
+    try {
+        const review = await Review.findByPk(reviewId);
+        if (!review) {
+            return res.status(404).json({ message: "Review not found" });
+        }
+
+        // Check if the Review belongs to the Current User
+        if (review.userId !== req.user.id) {
+            return res.status(403).json({ message: "Unauthorized. You can only modify your own reviews." });
+        }
+
+        await review.destroy();
+        return res.status(200).json({ message: "Review deleted successfully" });
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
